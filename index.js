e7e74edadcf4fefa7b04956dd50406bc2dc8d0d0
hxshan <galgodageheshan@gmail.com>
2023-02-17 15:04:44 +0530

    mobile almost complete

---
+++ index.js
@@ -0,0 +1,12 @@
+let menu_open_btn = document.querySelector(".menu-btn")
+let menu_close_btn =document.querySelector(".close-btn")
+let nav_items = document.querySelector("#nav-items")
+
+menu_open_btn.addEventListener('click',function(){
+    nav_items.classList.toggle("hidden")
+    nav_items.classList.add("nav-items")
+})
+menu_close_btn.addEventListener('click',function(){
+    nav_items.classList.toggle("nav-items")
+    nav_items.classList.add("hidden")
+})
